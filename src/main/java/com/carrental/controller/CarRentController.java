972f60fac6f7b3f894bd61daa66b0d1e11a892a2
Marcin Paszenda <marcinpaszenda@gmail.com>
2022-11-24 00:35:53 +0100

    update method in carRentService

--- src/main/java/com/carrental/controller/CarRentController.java
+++ src/main/java/com/carrental/controller/CarRentController.java
@@ -29,7 +29,6 @@ public class CarRentController {
     @GetMapping
     public ResponseEntity<List<CarRentDto>> getCarRents() {
         List<CarRent> carRents = carRentService.getAllCarRents();
-        carRentService.carRentActiveStatus(carRents);
         return ResponseEntity.ok(carRentMapper.mapToCarRentDtoList(carRents));
     }
 
